c0266927b37cc81c554d94aeda257a67ad736205
franblas <francois_blas@hotmail.com>
2014-12-06 00:32:33 +0100

    Update Configuration.java

--- Fra_part_Spectrogram/process/Configuration.java
+++ Fra_part_Spectrogram/process/Configuration.java
@@ -24,7 +24,7 @@ public class Configuration {
 	final public double HAAR_THRESHOLD = 0.05; //Threshold value for the selection after haar (has to be inferior to 1)
 	final public int FFT_POINTS_NB = 4096; //Number of points for each FFT (has to be power of 2) 
 	final public int STFT_FRAME_SIZE = 485; //Frame size of the STFT (in ms)
-	final public int FILE_SECONDS_SPLIT = 2; //Duration of each split wav file
+	final public int FILE_SECONDS_SPLIT = 2; //Length of each wav file split
 	final public int SIGNATURE_SIZE = 50; //Length of each fingerprint
 	final public int SIGNATURE_SUB_SIZE = 5; //Number of sub fingerprints
 	
